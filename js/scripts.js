af8c6d5742429272df4dee56490377e14acb614b
Kyle Lange and sheena nickerson <kyle@>
2016-08-09 14:08:47 -0700

    added javascript flashcards to the bottom of our html page.  We also added grid styling(via bootstrap.css) and .click interactivity to all buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,24 @@ $(function(){
     $("body").removeClass();
     $("body").addClass("green-background");
   });
+  $("#button1").click(function(){
+    $("#card1").toggle();
+  });
+  $("#button2").click(function(){
+    $("#card2").toggle();
+  });
+  $("#button3").click(function(){
+    $("#card3").toggle();
+  });
+  $("#button4").click(function(){
+    $("#card4").toggle();
+  });
+  $("#button5").click(function(){
+    $("#card5").toggle();
+  });
+  $("#button6").click(function(){
+    $("#card6").toggle();
+  });
 });
 
 
