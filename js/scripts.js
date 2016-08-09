11ce5e9eb5316e905c86d7b9e06b44a0aafa47b6
Kyle Lange and sheena nickerson <kyle@>
2016-08-09 10:33:12 -0700

    add images and basic structure to HTML. Also add jquery functions for clickable elements.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(function(){
+  $("#click1").click(function() {
+    $("#shy-owl").slideUp();
+  });
+  $("#click2").click(function() {
+    $("#hiding-image").slideDown();
+  });
+  $("#click3").click(function() {
+    $("#toggle-image").slideToggle();
+  });
+  $("#click4").click(function() {
+    $("#hidden-p").slideToggle();
+  });
+});
+
+
+// $("#hiding-image").show();
